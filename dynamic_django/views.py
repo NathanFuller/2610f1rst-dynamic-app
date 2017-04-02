92fc6540473eab7c331edb24294ebde20f40fb2c
Nate Fuller <nathan.fuller@att.net>
2017-04-02 23:41:03 +0000

    This is what I'm turning in.

--- dynamic_django/views.py
+++ dynamic_django/views.py
@@ -3,10 +3,11 @@ from django.http import HttpResponse
 
 
 #It would be really cool if this page were a dynamic list of the pages on my site.
-#Maybe figure that out.
+#Maybe figure that out sometime.
 def index(request):
     document = """
 <h3>This is the homepage to Nate's dynamic django web project.</h3>
+<p style='font-size: 8'>This page is not dynamic, unfortunately. But the others are!</p>
 <p>Links to the sites hosted here:</p>
 <ul>
 
