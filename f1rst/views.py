8773558f4ce22bdccace5d3582865948f6470b3b
Nate Fuller <nathan.fuller@att.net>
2017-04-02 23:34:05 +0000

    f1rst has a color calculated dynamically based on the server time

--- f1rst/views.py
+++ f1rst/views.py
@@ -5,7 +5,11 @@ import time
 
 
 def index(request):
-    return HttpResponse('<p>Hello, World...or something.</p><p>You accessed this page at '+time.strftime('%c')+'</p>')
+	hour = int(time.strftime('%H'))*11
+	minute =int(time.strftime('%M'))*4
+	sec = int(time.strftime('%S'))*4
+	style = "background-color:rgb("+str(hour)+","+str(minute)+","+str(sec)+ ");"
+    	return HttpResponse("<div style ='"+style +"'>"+ '<p>Hello, World...or something.</p><p>You accessed this page at '+time.strftime('%c')+'</p></div>')
 
 def diff(request):
 	return HttpResponse('<h1>This is a different page.</h1>')
